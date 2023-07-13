bff65b42941b8016d37c62ea2a4684af6955ba50
krishpranav <krisna.pranav@gmail.com>
2023-07-13 18:43:30 +0530

    Cano.JSON: Utils

---
+++ src/Cano.JSON/Utils.cs
@@ -0,0 +1,14 @@
+using System.Text;
+
+namespace Cano.JSON;
+
+static class Utils 
+{   
+    // Encoding[strict]
+    public static Encoding StrictUTF8 { get; }
+    
+    // Utility
+    static Utility()
+    {
+    }
+}
