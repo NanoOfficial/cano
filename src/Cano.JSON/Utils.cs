42c1aaba3a1e1eb6136bf8734c4bb785053d4489
krishpranav <krisna.pranav@gmail.com>
2023-07-13 18:45:59 +0530

    Cano.JSON: Utils

--- src/Cano.JSON/Utils.cs
+++ src/Cano.JSON/Utils.cs
@@ -2,13 +2,18 @@ using System.Text;
 
 namespace Cano.JSON;
 
-static class Utils 
-{   
-    // Encoding[strict]
+
+/// @brief: Utility
+static class Utility
+{
+    /// @brief: Encoding
     public static Encoding StrictUTF8 { get; }
-    
-    // Utility
+
+    /// @brief: Utility
     static Utility()
     {
+        StrictUTF8 = (Encoding)Encoding.UTF8.Clone();
+        StrictUTF8.DecoderFallback = DecoderFallback.ExceptionFallback;
+        StrictUTF8.EncoderFallback = EncoderFallback.ExceptionFallback;
     }
 }
