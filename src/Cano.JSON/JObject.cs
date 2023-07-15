d1363c6afae7deba0210228d4da604a9438bf41d
krishpranav <krisna.pranav@gmail.com>
2023-07-15 11:10:02 +0530

    Cano.JSON: JObject

--- src/Cano.JSON/JObject.cs
+++ src/Cano.JSON/JObject.cs
@@ -2,9 +2,9 @@ using System.Text.Json;
 
 namespace Cano.JSON
 {
-    public class JObject: JContainer
+    public class JObject : JContainer
     {
-        private readonly OrderedDictionary<string, JToken?>  properties_ = new();
+        private readonly OrderedDictionary<string, JToken?> properties = new();
 
         public IDictionary<string, JToken?> Properties => properties;
 
@@ -14,7 +14,6 @@ namespace Cano.JSON
             {
                 if (Properties.TryGetValue(name, out JToken? value))
                     return value;
-                
                 return null;
             }
             set
@@ -23,13 +22,39 @@ namespace Cano.JSON
             }
         }
 
-        public override IReadOnlyList<JToken?> Children => properties.Value;
+        public override IReadOnlyList<JToken?> Children => properties.Values;
 
         public bool ContainsProperty(string key)
         {
             return Properties.ContainsKey(key);
         }
 
-        public override void Clear() => Properties.Clear();
+        public override void Clear() => properties.Clear();
+
+        internal override void Write(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+            foreach (var (key, value) in Properties)
+            {
+                writer.WritePropertyName(key);
+                if (value is null)
+                    writer.WriteNullValue();
+                else
+                    value.Write(writer);
+            }
+            writer.WriteEndObject();
+        }
+
+        public override JObject Clone()
+        {
+            var cloned = new JObject();
+
+            foreach (var (key, value) in Properties)
+            {
+                cloned[key] = value != null ? value.Clone() : Null;
+            }
+
+            return cloned;
+        }
     }
 }
